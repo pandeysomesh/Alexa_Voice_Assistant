3f725a9243bc8c8fc687ba7aa6353d46ed4b0d42
pandeysomesh <pandeysomesh03@gmail.com>
2024-11-25 20:48:24 +0530

    Update Alexa App.py

--- Alexa App.py
+++ Alexa App.py
@@ -1,5 +1,3 @@
-
-#Importing Libraries
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
@@ -7,11 +5,7 @@ import datetime
 import wikipedia
 import pyjokes
 import requests, json , sys
-#PyAudio
-#PyWhatKit
-#PyJokes
-#Wikipedia
-#OpenweatherApi
+
 
 listener = sr.Recognizer()
 
@@ -24,61 +18,29 @@ def engine_talk(text):
     engine.runAndWait()
     
 def weather(city):
-    # Enter your API key here 
+     
     api_key = "<YOUR API KEY"
     
-    # base_url variable to store url 
+     
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-    # Give city name 
+     
     city_name = city
     
-    # complete_url variable to store 
-    # complete url address 
     complete_url = base_url + "appid=" + api_key + "&q=" + city_name 
     
-    # get method of requests module 
-    # return response object 
     response = requests.get(complete_url) 
     
-    # json method of response object  
-    # convert json format data into 
-    # python format data 
     x = response.json() 
     
-    # Now x contains list of nested dictionaries 
-    # Check the value of "cod" key is equal to 
-    # "404", means city is found otherwise, 
-    # city is not found 
     if x["cod"] != "404": 
     
-        # store the value of "main" 
-        # key in variable y 
         y = x["main"] 
     
-        # store the value corresponding 
-        # to the "temp" key of y 
         current_temperature = y["temp"] 
     
-        # store the value corresponding 
-        # to the "pressure" key of y 
-        #current_pressure = y["pressure"] 
-    
-        # store the value corresponding 
-        # to the "humidity" key of y 
-        #current_humidiy = y["humidity"] 
-    
-        # store the value of "weather" 
-        # key in variable z 
-        #z = x["weather"] 
-    
-        # store the value corresponding  
-        # to the "description" key at  
-        # the 0th index of z 
-        #weather_description = z[0]["description"]
         return str(current_temperature)
     
-        # print following values 
         '''print(" Temperature (in kelvin unit) = " +
                         str(current_temperature) + 
             "\n atmospheric pressure (in hPa unit) = " +
@@ -110,8 +72,6 @@ def run_alexa():
     command = user_commands()
     if 'play' in command:
         song = command.replace('play', '')
-        #print('New Command is' +command)
-        #print('The bot is telling us: Playing' +command)
         engine_talk('Playing' +song)
         pywhatkit.playonyt(song)
     elif 'time' in command:
@@ -127,7 +87,6 @@ def run_alexa():
     elif 'weather' in command:
         engine_talk('Please tell the name of the city')
         city = user_commands()
-        #weather_api = weather('Hong Kong')
         weather_api = weather(city)
         engine_talk(weather_api + 'degree fahreneit' )
     elif 'stop' in command:
